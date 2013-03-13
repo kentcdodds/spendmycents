ebcc8f72f138c23389d65604e85150ef01f58d33
Kent Dodds <me@kentcdodds.com>
2013-03-13 16:22:03 -0600

    Adding product search file. Likely to turn this into a controller of some kind...

---
+++ routes/products.js
@@ -0,0 +1,26 @@
+exports.search = function(req, res){
+	
+  OperationHelper = require('apac').OperationHelper;
+
+  var opHelper = new OperationHelper({
+      awsId: process.env.AMZ_ACCESS_KEY_CODE,
+      awsSecret: process.env.AMZ_SECRET_ACCESS_KEY,
+      assocId: process.env.AMZ_ASSOCIATE_ID
+  });
+
+  opHelper.execute('ItemSearch', {
+      'SearchIndex': 'All',
+      'Keywords': ' ',
+      'MaximumPrice': 12.50,
+      'MinimumPrice': 11.50,
+      'ResponseGroup': 'Medium'
+  }, function(error, results) {
+      if (error) {
+       res.json(500, {
+        error: "Something weird happened!",
+        message: error
+      });
+     }
+     res.send(results);
+  });
+};
