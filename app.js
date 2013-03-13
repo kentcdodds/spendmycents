7a3eacc28037e57dfb00e59650e4fd56372eb49d
Kent Dodds <me@kentcdodds.com>
2013-03-13 16:21:02 -0600

    Added products route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
+  , products = require('./routes/products')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
@@ -30,6 +31,7 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/products', products.search);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
