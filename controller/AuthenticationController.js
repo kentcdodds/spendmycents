7b4bf50be72d29d907c9cad53bfcfd92dc38a604
Kent Dodds <me@kentcdodds.com>
2013-03-15 21:23:01 -0600

    Added support for twitter and google. Also added a redirect for success/failure so we know whether it worked. Currently points to nothing, but good to know.

--- controller/AuthenticationController.js
+++ controller/AuthenticationController.js
@@ -8,6 +8,9 @@ var AuthenticationController
 AuthenticationController = {
   setupPassport: function() {
     configure.facebook();
+    configure.twitter();
+    configure.google();
+
     passport.serializeUser(function(user, done) {
       done(null, user.id);
     });
@@ -25,7 +28,7 @@ AuthenticationController = {
       authFunction(req, res);
     }
   },
-
+ 
   callback: function(req, res) {
     res.send('hello');
     var callbackFunction = callbackFrom[req.params.provider];
@@ -68,6 +71,40 @@ configure = {
         callbackURL: process.env.BASE_URL + "/auth/facebook/callback"
       },
       function(accessToken, refreshToken, profile, done) {
+        UserController.findOrCreate({id: profile.id, provider: profile.provider}, function(err, user) {
+          if (err) { return done(err); }
+          done(null, user);
+        });
+      }
+    ));
+  },
+
+  twitter: function() {
+    var TwitterStrategy = require('passport-twitter').Strategy;
+
+    passport.use(new TwitterStrategy({
+        consumerKey: process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: process.env.BASE_URL + "/auth/twitter/callback"
+      },
+      function(token, tokenSecret, profile, done) {
+        UserController.findOrCreate({id: profile.id, provider: profile.provider}, function(err, user) {
+          if (err) { return done(err); }
+          done(null, user);
+        });
+      }
+    ));
+  },
+
+  google: function() {
+    var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+    passport.use(new GoogleStrategy({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.BASE_URL + "/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
         UserController.findOrCreate({id: profile.id, provider: profile.provider}, function(err, user) {
           if (err) { return done(err); }
           done(null, user);
--- routes/AuthenticationRoutes.js
+++ routes/AuthenticationRoutes.js
@@ -7,9 +7,31 @@ authenticationRoutes = {
     app.get('/auth/facebook', passport.authenticate('facebook'));
     app.get('/auth/facebook/callback',
       passport.authenticate('facebook', {
-        successRedirect: '/',
-        failureRedirect: '/'
+        successRedirect: '/facebook-success',
+        failureRedirect: '/facebook-failure'
       }));
+
+    app.get('/auth/twitter', passport.authenticate('twitter'));
+    app.get('/auth/twitter/callback',
+      passport.authenticate('twitter', {
+        successRedirect: '/twitter-success',
+        failureRedirect: '/twitter-failure'
+      }));
+
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: [
+          'https://www.googleapis.com/auth/userinfo.profile',
+          'https://www.googleapis.com/auth/userinfo.email'
+        ]
+      })
+    );
+    app.get('/auth/google/callback',
+      passport.authenticate('google', {
+        successRedirect: '/google-success',
+        failureRedirect: '/google-failure'
+      }));
+
+
   }
 }
 
