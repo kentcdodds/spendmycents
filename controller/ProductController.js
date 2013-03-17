a1f04392c2e362e19f46c91fb2e81b038d9efb2d
Kent Dodds <me@kentcdodds.com>
2013-03-16 22:22:52 -0600

    Added option to include responseGroup in query parameter.

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -13,9 +13,9 @@ var ProductController = {
     opHelper.execute('ItemSearch', {
         'SearchIndex': req.query.searchIndex || 'All',
         'Keywords': req.query.keywords || ' ',
-        'MaximumPrice': req.query.maxPrice || req.query.minPrice || 1000,
-        'MinimumPrice': req.query.minPrice || req.query.maxPrice || 1000,
-        'ResponseGroup': 'Small,OfferSummary'
+        'MaximumPrice': req.query.price || req.query.maxPrice || req.query.minPrice || 1000,
+        'MinimumPrice': req.query.price || req.query.minPrice || req.query.maxPrice || 1000,
+        'ResponseGroup': req.query.responseGroup || 'Small,OfferSummary'
     }, function(error, results) {
         if (error) {
          res.json(500, {
