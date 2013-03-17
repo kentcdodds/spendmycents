109b7010e65d7ef30a2b4145dabb60bd7401390f
Kent Dodds <me@kentcdodds.com>
2013-03-16 20:19:29 -0600

    Apparently (http://stackoverflow.com/a/15456541/971592) the price is supposed to be in pennies, not dollars. So I've made the correction and also changed the ResponseGroup to what we need. Looks promising.

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -13,9 +13,9 @@ var ProductController = {
     opHelper.execute('ItemSearch', {
         'SearchIndex': req.query.searchIndex || 'All',
         'Keywords': req.query.keywords || ' ',
-        'MaximumPrice': req.query.maxPrice || 10,
-        'MinimumPrice': req.query.minPrice || 0,
-        'ResponseGroup': 'Medium'
+        'MaximumPrice': req.query.maxPrice || req.query.minPrice || 1000,
+        'MinimumPrice': req.query.minPrice || req.query.maxPrice || 1000,
+        'ResponseGroup': 'Small,OfferSummary'
     }, function(error, results) {
         if (error) {
          res.json(500, {
