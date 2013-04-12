65fadd0d023da084f368ea09a1eb21afff05b6ec
Kent Dodds <kent@doddsfamily.us>
2013-04-12 16:54:49 -0600

    Support for random results each identical request.

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -1,6 +1,7 @@
 var ProductController = (function() {
   var OperationHelper = require('apac').OperationHelper;
   var ErrorController = require('./ErrorController');
+  var _ = require('underscore');
   var instanceOpHelper;
   var getOpHelper;
   var defaultResponseGroups = 'Small,OfferSummary,Images'
@@ -18,17 +19,24 @@ var ProductController = (function() {
 
   return {
     searchProducts: function(req, res) {
+      var itemPage = req.query.itemPage || Math.floor(Math.random()*6);
       getOpHelper().execute('ItemSearch', {
         SearchIndex: req.query.searchIndex || 'All',
         Keywords: req.query.keywords || ' ',
         MaximumPrice: req.query.price || req.query.maxPrice || req.query.minPrice || 1000,
         MinimumPrice: req.query.price || req.query.minPrice || req.query.maxPrice || 1000,
-        ResponseGroup: req.query.responseGroup || defaultResponseGroups
+        ResponseGroup: req.query.responseGroup || defaultResponseGroups,
+        ItemPage: itemPage
       }, function(error, results) {
         if (error) {
           ErrorController.sendErrorJson(res, 500, error);
         } else {
-          res.send(results);
+          if (_.isEmpty(results) && itemPage !==1 && !req.query.hasOwnProperty('itemPage')) {
+            req.query.itemPage = 1;
+            this.searchProducts(req, res);
+          } else {
+            res.send(results);
+          }
         }
       });
     },
