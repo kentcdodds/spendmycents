f44c44269a94a9009e1db2025c3d444385b2f6f9
Kent Dodds <kent@doddsfamily.us>
2013-04-15 20:47:36 -0600

    Fixing searching "favorites" in search bar.

--- public/js/ProductView.js
+++ public/js/ProductView.js
@@ -1,20 +1,20 @@
-SMC.setupProductView = function (products) {
-  
+SMC.setupProductView = function(products) {
+
   var i;
   SMC.removeProductPanels();
   SMC.hideLoadingGif();
 
   for (i = 0; i < products.length; i += 1) {
-    var product, imageURL, title, manufacturer, detailPageURL, detailPageURLDescription, 
-        productPanelTemplate, saveAsFavoriteURL, favoritesLinkHTML;
-    
+    var product, imageURL, title, manufacturer, detailPageURL, detailPageURLDescription,
+      productPanelTemplate, saveAsFavoriteURL, favoritesLinkHTML;
+
     product = products[i];
-    
+
     //due to sometimes not receiving all the data we need to check for non-exisiting values
-    
+
     if (product.LargeImage && product.LargeImage[0].URL && product.LargeImage[0].URL[0]) {
       imageURL = product.LargeImage[0].URL[0];
-    } else if (product.ImageSets && product.ImageSets[0].ImageSet){
+    } else if (product.ImageSets && product.ImageSets[0].ImageSet) {
       try {
         imageURL = product.ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
       } catch (e) {
@@ -23,20 +23,20 @@ SMC.setupProductView = function (products) {
     } else {
       imageURL = "../public/img/No_image_available.png";
     }
-    
+
     if (product.ItemAttributes && product.ItemAttributes[0].Title) {
       title = product.ItemAttributes[0].Title[0];
       // if (title.length > )
     } else {
       title = "Title not provided.";
     }
-    
+
     if (product.ItemAttributes && product.ItemAttributes[0] && product.ItemAttributes[0].Manufacturer) {
       manufacturer = product.ItemAttributes[0].Manufacturer[0];
     } else {
       manufacturer = "Unknown";
     }
-    
+
     if (product.DetailPageURL) {
       detailPageURL = product.DetailPageURL[0];
       detailPageURLDescription = "See on Amazon";
@@ -44,77 +44,81 @@ SMC.setupProductView = function (products) {
       detailPageURL = "";
       detailPageURLDescription = "URL Unavailable";
     }
-    
+
     if (SMC.user) {
       var productId = product.ASIN[0];
-      
+
       favoritesLinkHTML = "&nbsp;|&nbsp;" +
-                      "<a href='#'onclick='SMC.setFavorite(\"" + 
-                      productId + "\")'>Save as Favorite</a>";
-      
+        "<a href='#'onclick='SMC.setFavorite(\"" +
+        productId + "\")'>Save as Favorite</a>";
+
     } else {
       favoritesLinkHTML = "";
     }
-    
-    
-    
+
+
+
     productPanelTemplate =
-        "<div id='" + product.ASIN[0] + "'class='hover product-panel'>" +
-            "<div class='front'>"+
-                "<img class=\"product-image thumb\" src=\"" + imageURL + "\">" +    
-            "</div>" + 
-            "<div class='back'>" +
-                "<div class='product-info'>" +
-                    "<p>" +
-                        "<span class='product-title'>" + title + "</span>" +
-                    "</p>" +
-                    "<p>" +
-                      "<span class='product-manufacturer'>Made By: &nbsp;" + manufacturer + "</span>" +
-                    "</p>" +
-                    "<p class='product-link'>" +
-                      "<a href=\"" + detailPageURL + "\" target=\"_blank\">" + detailPageURLDescription + "</a>" +
-                      favoritesLinkHTML +
-                    "</p>" +
-                "</div>"+
-            "</div>" + 
-        "</div>";
-    
-        
+      "<div id='" + product.ASIN[0] + "'class='hover product-panel'>" +
+      "<div class='front'>" +
+      "<img class=\"product-image thumb\" src=\"" + imageURL + "\">" +
+      "</div>" +
+      "<div class='back'>" +
+      "<div class='product-info'>" +
+      "<p>" +
+      "<span class='product-title'>" + title + "</span>" +
+      "</p>" +
+      "<p>" +
+      "<span class='product-manufacturer'>Made By: &nbsp;" + manufacturer + "</span>" +
+      "</p>" +
+      "<p class='product-link'>" +
+      "<a href=\"" + detailPageURL + "\" target=\"_blank\">" + detailPageURLDescription + "</a>" +
+      favoritesLinkHTML +
+      "</p>" +
+      "</div>" +
+      "</div>" +
+      "</div>";
+
+
     $('#product-container').append(productPanelTemplate);
   }
-  
+
 };
 
-SMC.sendSearchRequest = function (userInput) {
-  userInput = userInput * 100;
-  $('.product-panel').remove();
-  SMC.displayLoadingGif();
+SMC.sendSearchRequest = function(userInput) {
+  if (userInput.toString().toLowerCase() === 'favorites') {
+    SMC.loadUserFavorites();
+  } else {
+    userInput = userInput * 100;
+    $('.product-panel').remove();
+    SMC.displayLoadingGif();
 
-  $.ajax({  
-    type: "GET",  
-    url: "/products?price="+userInput,  
-    success: function(resp){
-    
-      var requests = 0;
-      SMC.response = resp;
-    
-      if(resp.ItemSearchResponse.Items[0].Item) {
-        SMC.setupProductView(resp.ItemSearchResponse.Items[0].Item);
-        SMC.setupHover();
-        requests += 1;
-      } else if (requests <= 5) {
-        SMC.sendSearchRequest();
+    $.ajax({
+      type: "GET",
+      url: "/products?price=" + userInput,
+      success: function(resp) {
+
+        var requests = 0;
+        SMC.response = resp;
+
+        if (resp.ItemSearchResponse.Items[0].Item) {
+          SMC.setupProductView(resp.ItemSearchResponse.Items[0].Item);
+          SMC.setupHover();
+          requests += 1;
+        } else if (requests <= 5) {
+          SMC.sendSearchRequest();
+        }
+
+      },
+      error: function(e) {
+
       }
-  
-    },  
-    error: function(e){  
-        
-    }  
-  });      
-}; 
+    });
+  }
+};
 
 
-SMC.shortenLongTitle = function (title) {
-  
+SMC.shortenLongTitle = function(title) {
+
 };
 
--- public/js/view.js
+++ public/js/view.js
@@ -1,4 +1,4 @@
-// Global object for 
+// Global object for
 var SMC = {};
 SMC.MAX_REQUEST_RETRIES = 3;
 
@@ -17,24 +17,24 @@ SMC.setupHover = function () {
 /******* Ajax Requests ********/
 
 SMC.sendRequest = function (){
-  var userInput, userInputIsValid; 
-  
+  var userInput, userInputIsValid;
+
   userInput = $('#user-input-price').val();
   userInputIsValid = SMC.validateUserInput(userInput);
-  
+
   if (userInputIsValid){
     SMC.sendSearchRequest(userInput);
   } else {
     userInput = userInput.toLowerCase();
-  
+
     if (_.isEqual(userInput, 'favorites')) {
       SMC.loadUserFavorites();
     } else {
       SMC.showAlert("error", "<strong>Warning!</strong> Easy there tiger. We only do numbers.");
     }
-  } 
-  
-  
+  }
+
+
 };
 
 
@@ -42,23 +42,23 @@ SMC.sendRequest = function (){
 
 /******** User authentication functions ********/
 
-SMC.checkIfUserIsLoggedIn = function () {  
-  $.ajax({  
-    type: "GET",  
-    url: "/users/me",  
+SMC.checkIfUserIsLoggedIn = function () {
+  $.ajax({
+    type: "GET",
+    url: "/users/me",
     success: function(resp){
-      
+
       if (resp['name']) {
         SMC.user = resp;
-        SMC.setupForUser(); 
+        SMC.setupForUser();
       } else if (resp['code'] === 403) {
         $('#favorites-button').hide();
       }
-    },  
-    error: function(e){  
-    }  
-  });     
-  
+    },
+    error: function(e){
+    }
+  });
+
 };
 /***********************************************/
 
@@ -66,7 +66,7 @@ SMC.checkIfUserIsLoggedIn = function () {
 
 SMC.validateUserInput = function (userInput) {
   console.log(userInput)
-  if ((userInput || userInput === "") && !_.isFinite(userInput)) {
+  if (((userInput || userInput === "") && !_.isFinite(userInput)) || userInput.toString().toLowerCase() === 'favorites') {
     return false;
   }
 
@@ -76,17 +76,17 @@ SMC.validateUserInput = function (userInput) {
 
 
 SMC.setupForUser = function () {
-  $('#user-status-button').html(SMC.user['name']);  
+  $('#user-status-button').html(SMC.user['name']);
   $('li.login').remove();
-  
+
   var logoutHTML = "<li class='logout'><span class='logout'><h5>All done? Click below to sign out.</h5></span></li>" +
                     "<li class='logout divider'></li>" +
-                    "<li class='logout'><span><a href='/auth/logout' id='logout-button' class='btn'>" + 
+                    "<li class='logout'><span><a href='/auth/logout' id='logout-button' class='btn'>" +
                         "Sign Out" +
                     "</a></span></li>";
-                    
+
   $('#login-dropdown').append(logoutHTML);
-  
+
 };
 
 SMC.displayLoadingGif = function () {
@@ -108,39 +108,39 @@ SMC.removeProductPanels = function () {
 SMC.showAlert = function (type, text) {
   var alertText = text;
   if (type === 'error') {
-    $(".alert").addClass('alert-error'); 
-  } 
+    $(".alert").addClass('alert-error');
+  }
   $('.alert').html(alertText);
   $(".alert").css('visibility', 'visible');
-   
-  window.setTimeout(function() { 
+
+  window.setTimeout(function() {
     $(".alert").css('visibility', 'hidden');
-    $(".alert").removeClass('alert-error'); 
+    $(".alert").removeClass('alert-error');
   }, 4000);
 };
 
 /******** App setup ********/
 
 $(document).ready(function () {
-  
+
   // Bind event handlers
   $('#user-input-price').keypress(function (e) {
     if (e.which == 13) {
       SMC.sendRequest();
     }
   });
-  
+
   $('#favorites-button').click(function () {
     $('#user-input-price').val('favorites');
     SMC.loadUserFavorites();
     console.log('calling loaduser favorites')
   });
-  
+
   $('#search-button').click(function () {
     SMC.sendRequest();
   })
-  
+
   SMC.checkIfUserIsLoggedIn();
-  
+
 });
 
