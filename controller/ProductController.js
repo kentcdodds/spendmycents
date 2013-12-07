e5f7913ca374a8c99c00989821e52cbd0d6f389f
Kent C. Dodds <kent@doddsfamily.us>
2013-12-06 22:56:12 -0700

    Adding an option that should have been there forever. Now when you hit /products all it sends you is products. Good gig am I right?

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -4,10 +4,10 @@ var ProductController = (function() {
   var OperationHelper = require('apac').OperationHelper;
   var ErrorController = require('./ErrorController');
   var _ = require('underscore');
-  var instanceOpHelper;
   var getOpHelper;
   var searchInputIsValid;
   var ifEachExistsAreValidNumbers;
+  var getSimplifiedResponse;
   var defaultResponseGroups = 'Small,OfferSummary,Images';
   var backupSearchIndexes = [
     "All",
@@ -96,6 +96,101 @@ var ProductController = (function() {
     return true;
   };
 
+  var ProductSetup = (function() {
+    var getProductImageUrl, getProductTitle, getProductManufacturer, getProductDetails, getASIN;
+
+    getProductImageUrl = function(product) {
+      var imageUrl = 'http://www.spendmycents.com/img/No_image_available.png';
+      if (product.LargeImage && product.LargeImage[0].URL && product.LargeImage[0].URL[0]) {
+        imageUrl = product.LargeImage[0].URL[0];
+      } else if (product.ImageSets && product.ImageSets[0].ImageSet) {
+        try {
+          imageUrl = product.ImageSets[0].ImageSet[0].LargeImage[0].URL[0];
+        } catch (e) {
+        }
+      }
+      return imageUrl;
+    };
+
+    getProductTitle = function(product) {
+      return product.ItemAttributes[0].Title[0] || 'Title not provided.';
+    };
+
+    getProductManufacturer = function(product) {
+      var manufacturer = 'Unknown';
+      if (product.ItemAttributes[0].hasOwnProperty('Manufacturer') &&
+        product.ItemAttributes[0].Manufacturer.length > 0) {
+
+        manufacturer = product.ItemAttributes[0].Manufacturer[0];
+      }
+      return manufacturer;
+    };
+
+    getASIN = function(product) {
+      var ASIN;
+      if (product.hasOwnProperty('ASIN') && product.ASIN.length > 0) {
+        ASIN = product.ASIN[0];
+      }
+      return ASIN;
+    };
+
+    return {
+      getProductFromAmazonResponsePortion: function(product) {
+        var imageUrl, title, manufacturer, url,  ASIN;
+
+        if (!product) {
+          return {};
+        }
+
+        imageUrl = getProductImageUrl(product);
+
+        if (product.hasOwnProperty('ItemAttributes') && product.ItemAttributes.length > 0) {
+          title = getProductTitle(product);
+          manufacturer = getProductManufacturer(product);
+        } else {
+          title = 'Title not provided';
+          manufacturer = 'Unknown';
+        }
+
+        if (product.hasOwnProperty('DetailPageURL') && product.DetailPageURL.length > 0) {
+          url = product.DetailPageURL[0];
+        }
+
+        ASIN = getASIN(product);
+
+        return {
+          ASIN: ASIN,
+          imageUrl: imageUrl,
+          title: title,
+          manufacturer: manufacturer,
+          url: url
+        };
+      }
+    };
+  })();
+
+  getSimplifiedResponse = function(resultsJSON) {
+    var itemsParent, amazonReponseItem, products = [];
+    if (resultsJSON.hasOwnProperty('ItemSearchResponse')) {
+      itemsParent = resultsJSON.ItemSearchResponse;
+    } else if (resultsJSON.hasOwnProperty('ItemLookupResponse')) {
+      itemsParent = resultsJSON.ItemLookupResponse;
+    }
+    if (itemsParent) {
+      if (itemsParent.hasOwnProperty('Items') &&
+        itemsParent.Items.length > 0 &&
+        itemsParent.Items[0].Item) {
+        amazonReponseItem = itemsParent.Items[0].Item;
+      }
+
+      for (var i = 0; i < amazonReponseItem.length; i++) {
+        var product = ProductSetup.getProductFromAmazonResponsePortion(amazonReponseItem[i]);
+        products.push(product);
+      }
+    }
+    return products;
+  };
+
   return {
     searchProducts: function(req, res) {
       if (!searchInputIsValid(req)) {
@@ -116,22 +211,27 @@ var ProductController = (function() {
         MinimumPrice: req.query.price || req.query.minPrice || req.query.maxPrice || 1000,
         ResponseGroup: req.query.responseGroup || defaultResponseGroups,
         ItemPage: itemPage
-      }, {
-        useXml: !!req.query.xml
-      }, function(results) {
-        if (!results) {
-          ErrorController.sendErrorJson(res, 500, error);
+      }, function(resultsJSON, resultsXML) {
+        if (!resultsJSON && !resultsXML) {
+          ErrorController.sendErrorJson(res, 500, 'No response from Amazon...');
         } else {
-          if (_.isEmpty(results) && itemPage !== 1 && !req.query.hasOwnProperty('itemPage')) {
+          if (_.isEmpty(resultsJSON) && itemPage !== 1 && !req.query.hasOwnProperty('itemPage')) {
             req.query.itemPage = 1;
             this.searchProducts(req, res);
           } else {
             if (req.query.xml) {
               res.set('Content-Type', 'application/xml');
+              res.send(resultsXML);
+            } else {
+              if (req.query.simplifyResponse) {
+                resultsJSON = getSimplifiedResponse(resultsJSON);
+              }
+              res.send(resultsJSON);
             }
-            res.send(results);
           }
         }
+      }, function(error) {
+        ErrorController.sendErrorJson(res, 500, error);
       });
     },
     getProducts: function(req, res) {
